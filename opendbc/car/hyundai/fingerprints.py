19e0b03dd94d368ac1556024cad659b587e32946
crwusiz <crwusiz@gmail.com>
2026-03-21 20:19:08 +0900

    update master

--- opendbc/car/hyundai/fingerprints.py
+++ opendbc/car/hyundai/fingerprints.py
@@ -617,6 +617,7 @@ FW_VERSIONS = {
     ],
     (Ecu.fwdCamera, 0x7c4, None): [
       b'\xf1\x00ME  MFC  AT KOR LHD 1.00 1.00 99211-GO000 241007',
+      b'\xf1\x00ME  MFC  AT KOR LHD 1.00 1.01 99211-GO000 250103',
     ],
   },
   CAR.HYUNDAI_IONIQ5_N: {  # (NE1)

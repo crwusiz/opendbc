5128b2a699fc05914e5c2cf5f212aeb81c870ae2
crwusiz <crwusiz@gmail.com>
2026-03-25 09:46:37 +0900

    fix

--- opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc/car/hyundai/hyundaicanfd.py
@@ -70,18 +70,17 @@ def create_steering_messages(packer, CP, CC, CS, CAN, frame, lat_active, apply_t
   # telling the ADAS ECU to forward our steering and disable stock LFA lane centering.
   ret = []
 
-  if CS.mdps is not None:
-    values = copy.copy(CS.mdps_info)
-    if angle_control:
-      if CS.lfa_alt_info is not None:
-        values["ADAS_ActiveStat_Lv2"] = CS.lfa_alt_info["ADAS_AngleActiveStat_Lv2"]
-    else:
-      if CS.lfa_info is not None:
-        values["LKA_ACTIVE"] = 1 if CS.lfa_info["STEER_REQ"] == 1 else 0
+  values = copy.copy(CS.mdps_info)
+  if angle_control:
+    if CS.lfa_alt_info is not None:
+      values["ADAS_ActiveStat_Lv2"] = CS.lfa_alt_info["ADAS_AngleActiveStat_Lv2"]
+  else:
+    if CS.lfa_info is not None:
+      values["LKA_ACTIVE"] = 1 if CS.lfa_info["STEER_REQ"] == 1 else 0
 
-    if frame % 1000 < 40:
-      values["OutTorque"] += 220
-    ret.append(packer.make_can_msg("MDPS", CAN.CAM, values))
+  if frame % 1000 < 40:
+    values["OutTorque"] += 220
+  ret.append(packer.make_can_msg("MDPS", CAN.CAM, values))
 
   if frame % 10 == 0:
     if CP.exFlags & HyundaiExFlags.HOD:
